a4af4628f48bde274770d14a6c53cadce3a222ba
Gabor Peter Suranyi <blindmancs@gmail.com>
2015-12-17 18:25:04 +0000

    Easier python execution
    Added bat file
    Cleaned up python code

---
+++ run_extract.bat
@@ -0,0 +1,1 @@
+C:\Python27\Python.exe wem-extract.py
--- wem-extract.py
+++ wem-extract.py
@@ -11,38 +11,45 @@ hashStorage = dict([])
 with open(config["paths"]["important"], 'r') as csvfile:
 	hashreader = csv.reader(csvfile, delimiter=',')
 	for row in hashreader:
-		#print ', '.join(row)
 		hashStorage[row[0]] = row[1]
 with open(config["paths"]["noise"], 'r') as csvfile:
 	hashreader = csv.reader(csvfile, delimiter=',')
 	for row in hashreader:
-		#print ', '.join(row)
 		hashStorage[row[0]] = row[1]
-
+# clear unknowns file
 open(config["paths"]["unknowns"], 'a').truncate()
 
 folder = config["paths"]["casc"]
+# run through the casc folder
 for dir in os.listdir(folder):
 	for file in os.listdir(folder+'/'+dir):
+		# grab all the files
 		if file.endswith(".xxx"):
 			path = folder+'/'+dir+'/'+file
 			with open(path, 'r') as f:
+				# if first line contains wave headers
 				first_line = f.readline()
 				if "WAVEfmt" in first_line[:20]:
+					# show some progress
 					if counter % 100 == 0:
 						print counter
 					counter = counter + 1
-
+					# convert to ogg
 					FNULL = open(os.devnull, 'w')
 					subprocess.call(config["paths"]["tools"]+'ww2ogg.exe '+path+' --pcb '+config["paths"]["tools"]+'packed_codebooks_aoTuV_603.bin', stdout=FNULL, stderr=subprocess.STDOUT)
+					# if convert was successful
 					if os.path.isfile(path.replace(".xxx", ".ogg")):
-						shutil.move(path.replace(".xxx", ".ogg"), config["paths"]["exported"]+str(counter)+".ogg")
 						temp_path = config["paths"]["exported"]+str(counter)+".ogg"
+						shutil.move(path.replace(".xxx", ".ogg"), temp_path)
+						# fix ogg
 						subprocess.call(config["paths"]["tools"]+'revorb.exe '+temp_path, stdout=FNULL, stderr=subprocess.STDOUT)
+						# check against hash storage
 						hash = hashlib.md5(temp_path).hexdigest()
 						if hash in hashStorage:
+							# move to a nice folder
 							shutil.move(temp_path, config["paths"]["exported"]+hashStorage[hash])
 						else:
+							# add hash to the unknowns list
 							unknown = unknown + 1
 							if(not config["full_extract"] and unknown == 1000):
 								sys.exit("1k unknowns listed. Please proceed to categorize. Ty! <3")
